0dab2683fcdce5ce128cffe3d3c5ff98409fa92c
Pako-12317 <patrykm10@nycstudents.net>
2025-01-16 14:27:15 +0000

    ,commit

--- src/main/java/com/example/project/IdGenerate.java
+++ src/main/java/com/example/project/IdGenerate.java
@@ -3,21 +3,21 @@ package com.example.project;
 public class IdGenerate{ //This class contains statics variable and methods, you do not initalize an object to use it.
     
     // //requires 1 private attribute String currentId. You must initialize it to 99
-    private String currentId = "99";
-    // //requires one empty constructor
-    public IdGenerate(){}
-
-    // public  getCurrentId(){}
-    public String getCurrentId(){
-        return currentId;
+    private static String currentId = "99";
+        // //requires one empty constructor
+        public IdGenerate(){}
+    
+        // public  getCurrentId(){}
+        public static String getCurrentId(){
+            return currentId;
     }
     // public  reset(){} //must reset the currentId back to 99
-    public void reset(){
+    public static void reset(){
         currentId = "99";
     }
 
     // public generateID(){} //generates a new id, when called it will increment the currentId by 1.. Hint pay attention to data type of currentId
-    public void generateID(){
+    public static void generateID(){
         Integer i = Integer.parseInt(currentId);
         i+=1;
         currentId = i.toString();
--- src/main/java/com/example/project/User.java
+++ src/main/java/com/example/project/User.java
@@ -1,49 +1,66 @@
 package com.example.project;
 
-public class User{
-    //requires 3 private attributes String name, String Id, Book book that is initialized to empty
+public class User {
     private String name;
     private String Id;
     private Book[] book = new Book[5];
-    //requires 1 contructor with two parameters that will initialize the name and id
-    public User(String name, String Id){
+
+    // Constructor to initialize name, Id, and book list
+    public User(String name, String Id) {
         this.name = name;
         this.Id = Id;
-    }
-    // public  getName() {}
-    public String getName(){
-        return name;
-    }
-    // public  setName() {}
-    public void setName(String name){
-        this.name = name;
-    }
-    // public  getId() {}
-    public String getId(){
-        return Id;
-    }
-    // public void setId() {}
-    public void setId(String Id){
-        this.Id = Id;
-    }
-    // public getBooks() {}
-    public Book[] getBooks(){
-        return book;
-    }
-    // public setBooks() {}
-    public void setBooks(Book[] books){
-        book = books;
-    }
-    // public String bookListInfo(){} //returns a booklist for the user, if empty, output "empty"
-    public String bookListInfo(){
-        if (book.equals(null)) {
-           return "empty"; 
-        } else {
-            return book.toString();  
+        for (int i = 0; i < book.length; i++) {
+            book[i] = new Book("empty", "empty", 0, "empty", 0); // Using Book's constructor with all attributes
         }
     }
-    // public String userInfo(){} //returns  "Name: []\nID: []\nBooks:\n[]"
-    public String userInfo(){
-        return "Name: " + name + "\nID: " + Id + "\nBooks:\n"+ book;
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter for Id
+    public String getId() {
+        return Id;
+    }
+
+    // Setter for Id
+    public void setId(String Id) {
+        this.Id = Id;
+    }
+
+    // Getter for books
+    public Book[] getBooks() {
+        return book;
+    }
+
+    // Setter for books
+    public void setBooks(Book[] books) {
+        this.book = books;
+    }
+
+    // Returns the book list as a formatted string
+    public String bookListInfo() {
+        StringBuilder bookInfo = new StringBuilder();
+        for (Book b : book) {
+            if (b != null && b.getTitle().equals("empty") && b.getAuthor().equals("empty")) {
+                bookInfo.append("empty\n");
+            } else if (b != null) {
+                bookInfo.append(b.bookInfo()).append("\n"); // Calls bookInfo() from Book class
+            } else {
+                bookInfo.append("empty\n");
+            }
+        }
+        return bookInfo.toString();
+    }
+
+    // Returns user information as a formatted string
+    public String userInfo() {
+        return "Name: " + name + "\nId: " + Id + "\nBooks: \n" + bookListInfo();
     }
 }
