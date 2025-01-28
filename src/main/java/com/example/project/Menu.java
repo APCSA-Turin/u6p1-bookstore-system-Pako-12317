47a10c7b232454a03af433e4969e8ccd71cfe243
Pako-12317 <patrykm10@nycstudents.net>
2025-01-28 23:26:13 +0000

    ,commit

--- src/main/java/com/example/project/Menu.java
+++ src/main/java/com/example/project/Menu.java
@@ -5,15 +5,62 @@ import java.util.Scanner;
 public class Menu {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("\033[H\033[2J");
-        System.out.println( "██████╗  ██████╗  ██████╗ ██╗  ██╗███████╗████████╗ ██████╗ ██████╗ ███████╗\r\n" + //
-                            "██╔══██╗██╔═══██╗██╔═══██╗██║ ██╔╝██╔════╝╚══██╔══╝██╔═══██╗██╔══██╗██╔════╝\r\n" + //
-                            "██████╔╝██║   ██║██║   ██║█████╔╝ ███████╗   ██║   ██║   ██║██████╔╝█████╗  \r\n" + //
-                            "██╔══██╗██║   ██║██║   ██║██╔═██╗ ╚════██║   ██║   ██║   ██║██╔══██╗██╔══╝  \r\n" + //
-                            "██████╔╝╚██████╔╝╚██████╔╝██║  ██╗███████║   ██║   ╚██████╔╝██║  ██║███████╗\r\n" + //
-                            "╚═════╝  ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚══════╝\r\n");
-        System.out.println("----------------------------------------------------------------------------\n" +
-                           "Press 0 to Exit\n" + "Press 1 to Add a new Book\n" + "Press 2 to Change Quantity of a Book\n" + "Press 3 to Search for a Book\n" + "Press 4 to Show Books\n" + "Press 5 to Add User\n" + "Press 6 to Show Users\n" + "Press 7 to Check Out Book\n" + "Press 8 to Check In Book");
-        input.nextInt();
+        BookStore store = new BookStore();
+        int in = 9;
+        String inString = "";
+        while(in != -1){
+            ASCII.printMenu();
+            in = input.nextInt();
+            input.nextLine();
+            if (in == 0) {
+                System.out.println("\033[H\033[2J");
+                break;
+            } else if(in == 1) {
+                System.out.println("Title: ");
+                String title = input.nextLine();
+                System.out.println("Author: ");
+                String author = input.nextLine();
+                System.out.println("When was the book published?");
+                int year = input.nextInt();
+                input.nextLine();
+                System.out.println("ISBN:");
+                String isbn = input.nextLine();
+                System.out.println("How many books?");
+                int quantity = input.nextInt();
+                input.nextLine();
+                Book book = new Book(title, author, year, isbn, quantity);
+                store.addBook(book);
+                in = 9;
+            } else if(in == 2) {
+                System.out.println("Which book would you like to change the quantity of?");
+                inString = input.nextLine();
+                System.out.println("What quantity should the book have?");
+                in = input.nextInt();
+                input.nextLine();
+                for (Book book : store.getBooks()) {
+                    if(book.getTitle() == inString){
+                        book.setQuantity(in);
+                    }
+                }
+            } else if(in == 3) {
+                System.out.println();
+            } else if(in == 4) {
+                System.out.println(store.getBooks());
+                Time.wait(5000);
+            } else if(in == 5) {
+                System.out.println();
+            } else if(in == 6) {
+                System.out.println();
+            } else if(in == 7) {
+                System.out.println();
+            } else if(in == 8) {
+                System.out.println();
+            } else {
+                ASCII.printInvalid();
+                Time.wait(1500);
+                ASCII.printMenu();
+            }
+        }
+        input.close();
     }
 }
